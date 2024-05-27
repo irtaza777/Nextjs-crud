443daef82b8ab9d4d7003b9f4079e6dd881d1e32
irtaza777 <irtazarizvi7@gmail.com>
2024-05-27 12:26:07 +0500

    navbar

---
+++ app/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from "next/link";
+import { BiAnchor } from "react-icons/bi";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className=" flex space-x-10 border-b-2 mb-5 h-14 items-center">
+<Link className='ml-8 text-3xl  ' href="Home"><BiAnchor />
+
+</Link>
+<ul className="flex space-x-6">
+    
+<li className=" cursor-pointer hover:text-red-700 transition-colors"><Link href="Home">Home</Link></li>
+    <li className=" cursor-pointer "><Link className='hover:text-red-700 transition-colors'  href="users">users</Link></li>
+    <li className="  cursor-pointer"><a href="">Login</a></li>
+    <li className=" text-orange-500 cursor-pointer"><a href="" className="font-bold">Logout</a></li>
+   </ul>
+
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ app/components/cart.jsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react'
+
+const cart = () => {
+  return (
+    <div>
+            <button onClick={()=>console.log("ik")}>Click</button>
+
+    </div>
+  )
+}
+
+export default cart
